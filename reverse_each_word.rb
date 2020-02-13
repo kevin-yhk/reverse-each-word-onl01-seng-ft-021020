e0e50e1f000586f9c63bf60bb12fb55636e65bd3
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-13 19:01:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,9 @@
-def reverse_each_word()
+def reverse_each_word(string)
+  string_original = string.split(" ")
+  new_sentence = []
+  
+  string_original.each do |word|
+    new_sentence << word.reverse
+  end
+  return new_sentence.join(" ")
+end
